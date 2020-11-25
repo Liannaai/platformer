b76cfe0ebe4d69a2822663849c876f66a496eb73
Liannaai <73580732+Liannaai@users.noreply.github.com>
2020-11-25 18:15:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
 namespace SpriteKind {
     export const Door = SpriteKind.create()
 }
+function chooseLevel () {
+    if (Level == 0) {
+        scene.setTileMap(list[Level])
+        createLevel()
+    } else if (Level == 1) {
+        deleteOldLevel()
+        scene.setTileMap(list[Level])
+        createLevel()
+    } else {
+        deleteOldLevel()
+        scene.setTileMap(list[Level])
+        createLevel()
+    }
+}
 function createLevel () {
     for (let value of scene.getTilesByType(8)) {
         scene.setTile(8, img`
@@ -73,16 +87,100 @@ function createLevel () {
             1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
             `, true)
     }
+    for (let value4 of scene.getTilesByType(1)) {
+        scene.setTile(1, img`
+            7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 5 7 5 5 7 7 7 7 7 7 7 7 7 7 
+            7 6 5 5 7 5 7 5 5 7 7 7 7 7 7 7 
+            7 7 6 5 7 7 5 5 6 7 7 7 7 7 7 7 
+            7 7 7 6 7 7 5 6 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 6 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 6 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 5 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            `, false)
+    }
+    for (let value4 of scene.getTilesByType(1)) {
+        scene.setTile(7, img`
+            d 1 d d d d d d d 1 d d d d d d 
+            d 1 d d d d d d d 1 d d d d d d 
+            d 1 d d d d d d d 1 d d d d d d 
+            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            d d d d d 1 d d d d d d d 1 d d 
+            d d d d d 1 d d d d d d d 1 d d 
+            d d d d d 1 d d d d d d d 1 d d 
+            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            d 1 d d d d d d d 1 d d d d d d 
+            d 1 d d d d d d d 1 d d d d d d 
+            d 1 d d d d d d d 1 d d d d d d 
+            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            d d d d d 1 d d d d d d d 1 d d 
+            d d d d d 1 d d d d d d d 1 d d 
+            d d d d d 1 d d d d d d d 1 d d 
+            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            `, false)
+    }
+    for (let value of scene.getTilesByType(15)) {
+        scene.setTile(15, img`
+            . . . . f . . . . 
+            . . . f f f . . . 
+            . . f c f c f . . 
+            . f c a f a c f . 
+            f f f f f f f f f 
+            . f c a f a c f . 
+            . . f c f c f . . 
+            . . . f f f . . . 
+            . . . . f . . . . 
+            `, false)
+        LUNCH = sprites.create(img`
+            . . . . f . . . . 
+            . . . f f f . . . 
+            . . f c f c f . . 
+            . f c a f a c f . 
+            f f f f f f f f f 
+            . f c a f a c f . 
+            . . f c f c f . . 
+            . . . f f f . . . 
+            . . . . f . . . . 
+            `, SpriteKind.Food)
+        scene.place(value, LUNCH)
+    }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.vy = -200
+    if (mySprite.vy == 0) {
+        mySprite.vy = -350
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Door, function (sprite, otherSprite) {
+    if (sprites.allOfKind(SpriteKind.Food).length == 0) {
+        if (Level == 2) {
+            game.over(true)
+        } else {
+            info.changeScoreBy(1)
+            mySprite.setPosition(30, 80)
+            createLevel()
+        }
+    }
 })
 function deleteOldLevel () {
     for (let value4 of sprites.allOfKind(SpriteKind.Player)) {
         value4.destroy()
     }
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy()
+})
+let LUNCH: Sprite = null
 let mySprite2: Sprite = null
+let Level = 0
+let list: Image[] = []
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -107,20 +205,66 @@ mySprite.ay = 350
 controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
 scene.setBackgroundColor(6)
-tiles.setTilemap(tiles.createTilemap(hex`1a000e0000000000000000000000000000000000000000000000000000000002000000010000000000000000000606000000000000000000000202020000000000060000020202030006000600010000000000000000000000060000000000000003030006000000010000000000010000000000000002020000030003030000000001010006000000000000000000000000000300000003000000000001000000000000000000020202000000030303030300000000060000000000000000000000000000000000000000000000000000000000000000000002020000000000000000000000000000000000000000000200000000000000000000000000000001010000000000000000000000000000010000000000000001010101000000000000000000000100000101010000000101010104050101000000000000000001010001010000000001010101010101010100000000000101010101010101010101010101010101010101010101010101`, img`
+scene.setTileMap(img`
     ..........................
-    .2...2..........2.........
-    .222........222..2.2.2....
-    ......................2...
-    ..2.......22..........22.2
-    .......................2..
-    .......222............2...
+    .....................f....
+    ..555555.....555555......8
+    ..555.....................
+    ..5.......................
+    ..5......555...f..5555....
+    .......55............5....
+    ......55..........55......
+    ..................5......5
+    .............222..........
+    ..55555......2222.........
+    ...........222222.........
+    .........2222f1111........
+    2222222222222222222222222f
+    `)
+list = [img`
+    ...............f.........8
     ..........................
-    .....22...................
-    ..2...............22......
-    ........2.......2222......
-    ....2..222....222..22.....
-    ...22.22....2.......2.....
+    .........5................
+    .....5...5................
+    .........5..555...........
+    ....5...55.......5........
+    .....5............55......
+    ..5..................55...
+    ..5555.....f..........5552
+    ........22222............2
+    .....222222222122..f.....2
+    .22222222222221222222....2
+    222222222f111f122222f12..2
     22222222222222222222222222
-    `, [myTiles.transparency16,sprites.builtin.forestTiles0,myTiles.tile3,sprites.dungeon.hazardLava0,myTiles.tile1,myTiles.tile2,myTiles.tile5], TileScale.Sixteen))
+    `, img`
+    ..........................
+    ...f......................
+    .........55........f......
+    .............55...........
+    .......55............57...
+    ...............55...558...
+    ....55....2..........55...
+    .........222......55.....f
+    ..55....2222..f...........
+    .......2221..........555..
+    ......22211222............
+    .....222f122222.....2.....
+    ....222222222222...2...2..
+    22222222222222222222222222
+    `, img`
+    ..f.......................
+    ..........................
+    ....55...2f22........555..
+    ........222222..55......55
+    ..f.....2222222..5....f..8
+    .......22222222..55.....57
+    .......222222222..........
+    .555..2222222222.....555..
+    ......2222f12222..........
+    ......2222211122...f......
+    .....22222221122..........
+    ....22222222f111..........
+    ...2222222212222.........f
+    22222222221222222222222222
+    `]
 createLevel()
